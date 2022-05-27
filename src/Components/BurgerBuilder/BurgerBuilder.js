0e120c75e9e208c8077678170c182deb0a4d629c
Mohammad Akbar <mohammad.akbar2603@gmail.com>
2022-05-27 19:18:02 +0600

    Initialized proj, added Navbar and Ingredients of Burger

---
+++ src/Components/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,19 @@
+import { Component } from "react";
+import Burger from "./Burger/Burger";
+
+export default class BurgerBuilder extends Component {
+    state = {
+        ingredients: [
+            { type: 'salad', amount: 0 },
+            { type: 'cheese', amount: 0 },
+            { type: 'meat', amount: 0 }
+        ]
+    }
+    render() {
+        return (
+            <div>
+                <Burger ingredients={this.state.ingredients} />
+            </div>
+        )
+    }
+}
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,33 @@
+import './Header.css';
+import Logo from '../../assets/logo.png';
+
+import {
+    Navbar,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+} from 'reactstrap';
+
+const Header = () => {
+    return (
+        <div className='Navigation'>
+            <Navbar style={{
+                backgroundColor: '#D70F64',
+                height: '70px',
+            }}>
+                <NavbarBrand href="/" className='mr-auto ml-md-5 Brand'>
+                    <img src={Logo} alt="Logo" width='80px' />
+                </NavbarBrand>
+                <Nav className='mr-md-5'>
+                    <NavItem>
+                        <NavLink href="#" className="NavLink">Something</NavLink>
+                    </NavItem>
+                </Nav>
+            </Navbar>
+        </div>
+    )
+}
+
+
+export default Header;
